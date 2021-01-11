9f46617ff45ac5fad8411bd7c15f1a31ccb5659d
Kentaro.Nishikata <kentaro.nishikata@gmail.com>
2021-01-11 14:40:08 +0900

    Initial Commit

---
+++ ImageFrameTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFrameTest
+//
+//  Created by 西方健太郎 on 2021/01/11.
+//  Copyright © 2021 idea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
